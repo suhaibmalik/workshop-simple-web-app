7913008e73e15cd05b105c3453c12ed9ebcde20e
Suhaib Malik <scrolltro0l@gmail.com>
2022-03-21 09:33:12 -0500

    Update main.go

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-// todo: create a file for handling the user list
+// todo: add an endpoint for handling the user list
 func main() {
 	http.HandleFunc("/current_time", currentTimeHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
